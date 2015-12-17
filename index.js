9601a87b2169b3f14ab9308361196b36009d89fa
ellisst <ellisst@tcd.ie>
2015-12-17 10:08:23 +0000

    set up the mongoose wiring in the project so now the central server is connecting to it

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
 var port = process.env.PORT || 7001;
 
-app.use(bodyParser.json());
-app.use(express.static(__dirname + "/frontend"));
+var db = mongoose.connect("mongodb://ellis:crEmona9@ds033125.mongolab.com:33125/micro_climate_monitor");
 
-app.post('/temperatureData', function (req, res) {
-  console.log(req.body);
-  res.send(req.body);
+db.connection.on('connected', function() {
+    app.use(bodyParser.json());
+    app.use(express.static(__dirname + "/frontend"));
+    app.post('/temperatureData', function (req, res) {
+      console.log(req.body);
+      res.send(req.body);
+    });
+    console.log(`Listening on port: ${port}`);
+    app.listen(port);
 });
 
-console.log(`Listening on port: ${port}`);
-app.listen(port);
+db.connection.on('error', function(err) {
+  console.log('Error connecting to: ' + connections.url + " " + err);
+});
+
+db.connection.on('disconnected', function() {
+  console.log('Mongoose default connection disconnected');
+});
+
+
+
