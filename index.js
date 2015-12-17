f1683d15c45b67a442b53c7e61f9d1bda35d8aba
John Orr <john.orr@bearingpoint.com>
2015-12-17 12:33:55 +0000

    adding more error checking

--- index.js
+++ index.js
@@ -18,33 +18,29 @@ db.connection.on('connected', function() {
     });
     var Reading = mongoose.model('Reading', readingSchema);
     app.post('/temperatureData', function (req, res) {
-        console.log(req.body);
-        //parser
-        var tempData = new Reading();
-        var timestamp = Date.parse(req.body.timestamp);
-        if (req.body.name && timestamp && req.body.temperature)
-            {
-                tempData.name = req.body.name;
-                tempData.timestamp = timestamp;
-                tempData.temperature = req.body.temperature;
-                //taking the data from temperatureDate and logging it into the schema
-                console.log("saving: " + tempData); //print which tempData is being saved
-                //final step to send to db
-                tempData.save(function(err, success) {
-                    if (err) {
-                        res.send("general error!");
-                    } else {
-                        res.send("saved successfully" + success);
-                    }
-                });   
-            }
+        if (req.body.name && Date.parse(req.body.timestamp) && req.body.temperature) {
+        	var tempData = new Reading();
+		  	tempData.name = req.body.name;
+            tempData.timestamp = req.body.timestamp;
+            tempData.temperature = req.body.temperature;
+            //taking the data from temperatureDate and logging it into the schema
+            console.log("Saving: " + tempData); //print which tempData is being saved
+            //final step to send to db
+            tempData.save(function(err, success) {
+                if (err) {
+                    res.send("General error!");
+                } else {
+				    console.log("Saved successfully:" + success)
+                    res.send("Saved successfully:" + success);
+                }
+            });   
+        } else {
+		  res.send(`Invalid data: ${JSON.stringify(req.body)}`);
+		}
     });
     console.log(`Listening on port: ${port}`);
     app.listen(port);
 });
-
-
-
 
 db.connection.on('error', function(err) {
   console.log('Error connecting to: ' + connections.url + " " + err);
