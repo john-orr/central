4b891e8e3a2346110294b8b6d72ae20f6445f506
John Orr <john.orr@bearingpoint.com>
2015-12-21 10:01:28 +0000

    changing name to location and filtering database query to limit results to valid locations

--- daos/readingsDao.js
+++ daos/readingsDao.js
@@ -1,28 +1,27 @@
 var Reading = require('../models/reading');
 
 function ReadingsDao() {
-
-    if (false === (this instanceof ReadingsDao)) {
-            console.log('Warning: ReadingsDao constructor called without "new" operator');
-            return new ReadingsDao();
-     }
-
-    this.createReading = function(data, callback) {
-        var reading = new Reading();
-        reading.name = data.name;
+	
+	if (false === (this instanceof ReadingsDao)) {
+		console.log('Warning: ReadingsDao constructor called without "new" operator');
+		return new ReadingsDao();
+	}
+	
+	this.createReading = function(data, callback) {
+		var reading = new Reading();
+        reading.location = data.location;
         reading.timestamp = data.timestamp;
         reading.temperature = data.temperature;
         reading.save(function(err, data) {
-             callback(err, data);
+			callback(err, data);
         });
-    }
-
-     this.getReadings = function(callback) {
-        Reading.find({}, function(err, readings) {
-              callback(err, readings);
-        });
-     }
-
+	}
+	
+	this.getReadings = function(callback) {
+		Reading.find({ location: /^[A-J]$/ }, function(err, readings) {
+			callback(err, readings);
+		});
+	}
 }
 
 module.exports.ReadingsDao = ReadingsDao;
--- routes/routes.js
+++ routes/routes.js
@@ -1,34 +1,34 @@
 var ReadingsDao = require('../daos/readingsDao').ReadingsDao;
 
 module.exports = function(app, server) {
-
-var readingsDao = new ReadingsDao();
-var io = require('socket.io').listen(server);
+	
+	var readingsDao = new ReadingsDao();
+	var io = require('socket.io').listen(server);
 
     app.post('/temperatureData', function (req, res) {
-            console.log(`${req.body.length} records received`);
-            var validRecords = 0;
-            for(i in req.body) {
-                data = req.body[i];
-                if (data.name && Date.parse(data.timestamp) && data.temperature) {
-                    validRecords++;
-                    readingsDao.createReading(data, function(err, reading) {
-                        if (err) {
-                            res.send("General error!");
-                        } else {
-                            io.sockets.emit('newReading', reading);
-                            console.log("Saved successfully:" + reading);
-                        }
-                    });
-                }
-            }
-               console.log(`${validRecords} valid records`)
-               res.send(`Thanks for the data! ${validRecords} were valid`);
-
-        });
-    app.get('/pastTemperatures', function(req, res) {
-       readingsDao.getReadings(function(err, readings) {
-            res.json(readings);
-       });
-    });
+		console.log(`${req.body.length} records received`);
+		var validRecords = 0;
+		for(i in req.body) {
+			data = req.body[i];
+			if (data.location && Date.parse(data.timestamp) && data.temperature) {
+				validRecords++;
+				readingsDao.createReading(data, function(err, reading) {
+					if (err) {
+						res.send("General error!");
+					} else {
+						io.sockets.emit('newReading', reading);
+						console.log("Saved successfully:" + reading);
+					}
+				});
+			}
+		}
+		console.log(`${validRecords} valid records`)
+		res.send(`Thanks for the data! ${validRecords} were valid`);
+	});
+	
+	app.get('/pastTemperatures', function(req, res) {
+		readingsDao.getReadings(function(err, readings) {
+			res.json(readings);
+		});
+	});
 }
